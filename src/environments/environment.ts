616513120c3b0809a921992029ab4a40adad0c9f
Andy Edimir <andyancajima@unilene.com>
2022-03-17 14:35:21 -0500

    Envorment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  // urlApiSatelliteCore : 'http://localhost:56659',
-   urlApiSatelliteCore : 'http://172.168.10.22:8080/SatelliteCore',
+  urlApiSatelliteCore : 'http://localhost:56659',
+  //  urlApiSatelliteCore : 'http://172.168.10.22:8080/SatelliteCore',
   secretKeyEncryption: 'NY@vkxvME8xB*%5v1j84QxCVHNeDKcnuo$j84*T',
   secretKeyHMAC: 'hV#RobuI6fsHRv&^!VJxdO1sa4ZhCdhsnHlR@lm'
 };
 
-/*
+/*ng 
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
  * import the following file, but please comment it out in production mode
