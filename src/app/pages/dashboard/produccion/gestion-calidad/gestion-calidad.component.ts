f06d48b7c2624ddac4179695944b65ea092f95a8
Andy Edimir <andyancajima@unilene.com>
2022-12-12 15:23:14 -0500

    PPSTI-132

---
+++ src/app/pages/dashboard/produccion/gestion-calidad/gestion-calidad.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-calidad',
+  templateUrl: './gestion-calidad.component.html'
+})
+export class GestionCalidadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
