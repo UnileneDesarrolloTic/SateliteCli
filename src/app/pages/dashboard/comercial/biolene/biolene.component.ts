dae6ecbc063368c9883d112682b68f8160fd1f22
AndyUnilene <andyancajima@unilene.com>
2023-02-07 14:41:20 -0500

    PPSTI-153

---
+++ src/app/pages/dashboard/comercial/biolene/biolene.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-biolene',
+  templateUrl: './biolene.component.html',
+})
+export class BioleneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
