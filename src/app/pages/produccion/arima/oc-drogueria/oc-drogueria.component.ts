073840ada1523acbc714efe7eecf7190009d18af
AndyUnilene <andyancajima@unilene.com>
2023-05-04 08:16:50 -0500

    PPST

--- src/app/pages/produccion/arima/oc-drogueria/oc-drogueria.component.ts
+++ src/app/pages/produccion/arima/oc-drogueria/oc-drogueria.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MostrarOrdenCompraDrogueria } from '@data/interface/Response/OCDrogueria/DatosFormatoMostrarOrdenCompra.interface';
 import { MostrarProveedorDrogueria } from '@data/interface/Response/OCDrogueria/DatosFormatoMostrarProveedor.interface';
@@ -6,13 +6,11 @@ import { OrdenCompraPrevio } from '@data/interface/Response/OCDrogueria/DatosFor
 import { ModelSeguimientoDrogueria } from '@data/interface/Response/OCDrogueria/DatosFormatoSeguimientoDrogueria.interface';
 import { ProduccionService } from '@data/services/backEnd/pages/produccion.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ModalCargarComponent } from '@shared/components/modal-cargar/modal-cargar.component';
-import { Cargarbase64Service } from '@shared/services/comunes/cargarbase64.service';
 import { ToastrService } from 'ngx-toastr';
 import { debounceTime } from 'rxjs/operators';
-import { GenericoService } from '@shared/services/comunes/generico.service';
 import { ModalVerTransitoComponent } from '@shared/components/modal-ver-transito/modal-ver-transito.component';
 import { FullComponent } from '@layout/full/full.component';
+import { FileService } from '@shared/services/comunes/file.service';
 
 @Component({
   selector: 'app-oc-drogueria',
@@ -24,34 +22,34 @@ export class OcDrogueriaComponent implements OnInit {
   listarProductoDrogueria: any;
   listarItemDrogueria: ModelSeguimientoDrogueria[] = [];
   templistarItemDrogueria: ModelSeguimientoDrogueria[] = [];
-  
+
   listarOrdenCompraDrogueria: MostrarOrdenCompraDrogueria[] = [];
   proveedores: MostrarProveedorDrogueria[] = [];
-  ordenCompraPrevio:OrdenCompraPrevio[]=[];
+  ordenCompraPrevio: OrdenCompraPrevio[] = [];
 
   itemModalOC: string = "";
   formularioFiltro: FormGroup;
   flagEspera: boolean = false;
-  flagEsperaExcel:boolean=false;
-  
+  flagEsperaExcel: boolean = false;
+
   checkMostrarColumna = new FormControl(false);
   textFiltrar = new FormControl('');
   reporteDrogueria = new FormControl('sinGrupo');
+  flagEsperaExcelAgrupador: boolean = false;
 
 
   constructor(private _ProduccionService: ProduccionService,
     private _modalService: NgbModal,
     private _toastr: ToastrService,
-    private _Cargarbase64Service:Cargarbase64Service,
-    private _GenericoService : GenericoService,
-    private _fullcomponent : FullComponent) { 
-      this._fullcomponent.options.sidebartype = 'mini-sidebar'
+    private _FileService: FileService,
+    private _fullcomponent: FullComponent) {
+    this._fullcomponent.options.sidebartype = 'mini-sidebar'
 
-    }
+  }
 
   ngOnInit(): void {
     // this.accesosPermiso();
-   
+
     this.filtroFormulario();
     this.mostrarProveedor();
     this.reporteSeguimientoDrogueria();
@@ -60,8 +58,8 @@ export class OcDrogueriaComponent implements OnInit {
     this.generarOC();
   }
 
-  ngAfterViewInit(){
-    
+  ngAfterViewInit() {
+
   }
 
   filtroFormulario() {
@@ -70,13 +68,13 @@ export class OcDrogueriaComponent implements OnInit {
     })
   }
 
-  isObservableFiltro(){
-    this.textFiltrar.valueChanges.pipe(debounceTime(900)).subscribe(valorBusqueda=>{
-          this.filtrar(valorBusqueda);
+  isObservableFiltro() {
+    this.textFiltrar.valueChanges.pipe(debounceTime(900)).subscribe(valorBusqueda => {
+      this.filtrar(valorBusqueda);
     })
   }
 
-  filtrar(valorBusqueda){
+  filtrar(valorBusqueda) {
     const inputText = valorBusqueda.toLowerCase().trim();
     this.listarItemDrogueria = this.templistarItemDrogueria.filter(element => element.item.toLowerCase().indexOf(inputText) !== -1 || element.descProveedor?.toLowerCase().indexOf(inputText) !== -1 || element.descripcionLocal?.toLowerCase().indexOf(inputText) !== -1);
   }
@@ -90,10 +88,10 @@ export class OcDrogueriaComponent implements OnInit {
       (resp: any) => {
         if (resp["success"]) {
           this.listarItemDrogueria = resp["content"];
-          this.templistarItemDrogueria=resp["content"];
+          this.templistarItemDrogueria = resp["content"];
         } else {
           this.listarItemDrogueria = resp["content"];
-          this.templistarItemDrogueria=resp["content"];
+          this.templistarItemDrogueria = resp["content"];
           this._toastr.info(resp["message"]);
         }
         this.flagEspera = false;
@@ -110,7 +108,7 @@ export class OcDrogueriaComponent implements OnInit {
     )
   }
 
-  abrirModalMostrarOC(Item : string) {
+  abrirModalMostrarOC(Item: string) {
     const ModalTransito = this._modalService.open(ModalVerTransitoComponent, {
       windowClass: 'my-class',
       centered: true,
@@ -119,30 +117,28 @@ export class OcDrogueriaComponent implements OnInit {
       scrollable: true
     });
     ModalTransito.componentInstance.Item = Item;
-    ModalTransito.result.then((result) => 
-    {
-      
+    ModalTransito.result.then((result) => {
+
     }, (refrescado) => {
-      if(refrescado > 0)
-      {
+      if (refrescado > 0) {
         this.reporteSeguimientoDrogueria();
       }
     });
   }
 
 
-    generarOrdenCompra(){
-      this._ProduccionService.generarOrdenCompraPrevios().subscribe(
-        (resp:any)=>{
-            this.ordenCompraPrevio=resp["content"];
-        },
-      
-      )
-    }
-  
+  generarOrdenCompra() {
+    this._ProduccionService.generarOrdenCompraPrevios().subscribe(
+      (resp: any) => {
+        this.ordenCompraPrevio = resp["content"];
+      },
 
-  verTransito(){
-    this.listarOrdenCompraDrogueria=[];
+    )
+  }
+
+
+  verTransito() {
+    this.listarOrdenCompraDrogueria = [];
     const ModalTransito = this._modalService.open(ModalVerTransitoComponent, {
       windowClass: 'my-class',
       centered: true,
@@ -152,37 +148,34 @@ export class OcDrogueriaComponent implements OnInit {
     });
 
     ModalTransito.componentInstance.Item = '';
-    ModalTransito.result.then((result) => 
-    {
-      
+    ModalTransito.result.then((result) => {
+
     }, (refrescado) => {
-      if(refrescado > 0)
-      {
+      if (refrescado > 0) {
         this.reporteSeguimientoDrogueria();
       }
     });
   }
 
 
-  generarOC(){
+  generarOC() {
     this._ProduccionService.generarOrdenCompraDrogueria().subscribe(
-        (resp:any)=>{
-              if(resp["success"])
-              {
-                this._toastr.success(resp["message"])
-                this.listadoOrdenCompra();
-              }
+      (resp: any) => {
+        if (resp["success"]) {
+          this._toastr.success(resp["message"])
+          this.listadoOrdenCompra();
         }
+      }
     );
-   }
+  }
 
 
-   listadoOrdenCompra(){
+  listadoOrdenCompra() {
     this._ProduccionService.generarOrdenCompraPrevios().subscribe(
-      (resp:any)=>{
-          this.ordenCompraPrevio=resp["content"];
+      (resp: any) => {
+        this.ordenCompraPrevio = resp["content"];
       },
-    
+
     )
   }
 
@@ -195,36 +188,24 @@ export class OcDrogueriaComponent implements OnInit {
       size: 'sm',
       scrollable: true
     });
-    
-    
 
-    }
+  }
 
-    aceptarDescarga(){
-        this.flagEsperaExcel=true;
-        const ModalCarga = this._modalService.open(ModalCargarComponent, {
-          centered: true,
-          backdrop: 'static',
-          size: 'sm',
-          scrollable: true
-        });
-        ModalCarga.componentInstance.fromParent = "Generando el Formato Excel";
-        this._ProduccionService.exportarCompraDrogueria(this.formularioFiltro.controls.idproveedor.value,this.checkMostrarColumna.value, this.reporteDrogueria.value).subscribe(
-          (resp:any)=>{
-            if(resp.success){
-              this._Cargarbase64Service.file(resp.content,`CompraDrogueria-${this.hoy}`,'xlsx',ModalCarga);
-            }else{
-              ModalCarga.close();
-              this._toastr.info(resp.message);
-            }
-            this.flagEsperaExcel=false;
-          },
-          _=> {
-                ModalCarga.close();
-                this.flagEsperaExcel=false;
-          }
-        );
-    }
+  aceptarDescarga() {
+    this.flagEsperaExcelAgrupador = true;
+
+    this._ProduccionService.exportarCompraDrogueria(this.formularioFiltro.controls.idproveedor.value, this.checkMostrarColumna.value, this.reporteDrogueria.value).subscribe(
+      (resp: any) => {
+        if (resp.success) {
+          this._FileService.decargarExcel_Base64(resp.content, `CompraDrogueria-${this.hoy}`, 'xlsx');
+        } else {
+          this._toastr.info(resp.message);
+        }
+        this.flagEsperaExcelAgrupador = false;
+      },
+      _ => this.flagEsperaExcelAgrupador = false
+    );
+  }
 
 
 
