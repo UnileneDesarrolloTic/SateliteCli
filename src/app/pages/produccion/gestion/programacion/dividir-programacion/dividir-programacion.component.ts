368054151f5b0f8b0a3c6b206d98ff2c4831215c
AndyUnilene <andyancajima@unilene.com>
2023-07-13 08:24:04 -0500

    PPST

--- src/app/pages/produccion/gestion/programacion/dividir-programacion/dividir-programacion.component.ts
+++ src/app/pages/produccion/gestion/programacion/dividir-programacion/dividir-programacion.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ProgramacionOperacionesOrdenFabricacion } from '@data/interface/Response/ProgramacionOperaciones/DatosFormatoProgramacionOperaciones.interface';
 import { ProgramacionOperacionesService } from '@data/services/backEnd/pages/programacion-operaciones.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
@@ -22,25 +22,42 @@ export class DividirProgramacionComponent implements OnInit {
   ngOnInit(): void {
     
     this.crearFormulario();
+    this.form.patchValue({
+      ordenFabricacion: this.paramentros.ordenFabricacion
+    })
   }
 
   crearFormulario() {
     this.form = new FormGroup({
-      items: new FormArray([])
+      ordenFabricacion: new FormControl('',Validators.required),
+      items: new FormArray([this.createDivision()])
     })
   }
 
   createDivision(): FormGroup {
     return this._fb.group({
-      cantidad: '',
-      fecha: '',     
+      cantidad: [0,Validators.required],
+      fecha: [null,Validators.required],     
     });
   }
 
   agregarDivision(): void {
-    console.log("agregarDivision");
     this.items = this.form.get('items') as FormArray;
     this.items.push(this.createDivision());
+  }
+  
+
+  registrarDivisionProgramacion(){
+     console.log(this.form.value);
+  }
+
+  formItemDividir():FormArray {
+    return this.form.get('items') as FormArray;
+  }
+
+  eliminacionDivision(index:number) {
+    const itemDividir =  this.form.get('items') as FormArray;
+    itemDividir.removeAt(index);
   }
 
 }
