5236e74629fd4fcd9529f2b083f355057ac44b0d
AndyUnilene <andyancajima@unilene.com>
2023-07-18 18:33:13 -0500

    PPSTI-222

--- src/app/data/interface/Response/Dispensacion/DatosFormatoListadoHistorial.interfaces.ts
+++ src/app/data/interface/Response/Dispensacion/DatosFormatoListadoHistorial.interfaces.ts
@@ -3,6 +3,7 @@ export interface HistorialDispensacion {
     documento:          string;
     secuencia:          number;
     item:               string;
+    descripcion:        string;
     lote:               string;
     cantidadIngresada:  number;
     cantidadSolicitada: number;
